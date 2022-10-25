dc69732b20666ac4f900bf41aaa1e78db5b40824
taher salah <tahersalah2016@gmail.com>
2022-10-25 21:28:00 +0200

    first commit

---
+++ lib/moduals/on_boarding/on_boarding_screen.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+//
+// class OnBoarding extends StatelessWidget {
+//   const OnBoarding({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Column(
+//         children: const [
+//         Image(image: Image.asset(''));
+//         ],
+//       ),
+//     );
+//   }
+// }
